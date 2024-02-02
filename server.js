efa1341618f1294091bc8f0b9a6fca9f94c5e499
mikesabzz <michaelsabzevari@gmail.com>
2024-02-01 23:03:15 -0500

    server

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const path = require('path')
-require('dotenv').config()
 
 const authRouter = require('./router/authRouter')
 const appRouter = require('./router/appRouter')
 const { authorized } = require('./auth/auth')
 const passport = require('passport')
-
+require('dotenv').config()
 
 // establishing the I/O port
 const PORT = process.env.PORT || 4567
@@ -31,13 +30,13 @@ app.use(logger('dev'))
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(bodyParser.json())
 // Static hosting for built files
-app.use(express.static(path.join(__dirname, './client/build')));
-
 app.use('/auth', authRouter)
 app.use('/app',  authorized, appRouter) // to reactive security
 app.use(passport.initialize())
+
 app.get('/', async (request, response) => {
   try {
     response.sendFile(path.join(__dirname, './client/build', 'index.html'));
@@ -49,19 +48,17 @@ app.get('/', async (request, response) => {
 app.get('/error', function(req, res){
   res.render('error.html');
 });
-// In production, any request that doesn't match a previous route
-// should send the front-end application, which will handle the route.
-if (process.env.NODE_ENV == "production") {
-// if (process.env.NODE_ENV == "development") {
-  app.use('*', (req, res) => res.sendFile(path.join(__dirname, './client/build', "index.html")));
-}
-
-
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message})
 })
+app.use(express.static(path.join(__dirname, './client/build')));
 
-
+// In production, any request that doesn't match a previous route
+// should send the front-end application, which will handle the route.
+// if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "development") {
+//   app.use('*', (req, res) => res.sendFile(path.join(__dirname, './client/build', "index.html")));
+// }
 
 /*
 // Generic error handler
